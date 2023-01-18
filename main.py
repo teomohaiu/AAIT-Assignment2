d3bf2834a081c9aead3296aaab1bdd9c6d870b3b
Mohaiu Teodora <mohaiuantoniateodora@yahoo.com>
2023-01-18 19:11:50 +0200

    Modified training routine taking into consideration checkpoints of the models.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from dataset.Dataset import ImageDataset, get_transforms
 from trainer.train_pseudo_labelling import train_supervised, evaluate, train_semisupervised, evaluate_for_submission
 from models.simple_convnet import Net
 from models.simple_convnet_2 import ConvNet
+from models.resnet50 import resnet50_model
 import matplotlib.pyplot as plt
 import yaml
 import datetime
@@ -16,10 +17,12 @@ import pandas as pd
 def get_arguments():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--batch_size', type=int, default=4, help='Batch size (default: 4)')
+    parser.add_argument('--batch_size', type=int, default=8, help='Batch size (default: 8)')
+    parser.add_argument('--checkpoint_supervised_model', type=str, default=None, help='Checkpoint path for the supervised trained model')
+    parser.add_argument('--checkpoint_semi_supervised_model', type=str, default=None, help='Checkpoint path for the semi-supervised trained model')
     parser.add_argument('--dataset', type=str, default='./task1/')
-    parser.add_argument('--model', type=str, default='simple_convnet_2')
-    parser.add_argument('--epochs', type=int, default=50,  help='Number of training epochs (default: 50)')
+    parser.add_argument('--model', type=str, default='resnet50')
+    parser.add_argument('--epochs', type=int, default=100,  help='Number of training epochs (default: 50)')
     parser.add_argument('--learning_rate', type=float, default=0.001,  help='Learning rate value')
     parser.add_argument('--momentum', type=float, default=0.9,  help='Momentum for SGD optimizer')
     parser.add_argument('--device', default='cuda', type=str,
@@ -27,7 +30,7 @@ def get_arguments():
 
     parser.add_argument('--problem_type', type=str, default='missing-labels', choices=['missing-labels', 'noisy-labels'], help='Wheter to use or not the unlabeled data')
     parser.add_argument('--loss_function', type=str, default='cross-entropy', choices=['cross-entropy', 'negative-loglikelihood'], help='Type of loss to use')
-    parser.add_argument('--optimizer', type=str, default='sgd', choices=['sgd', 'adam'], help='Type of optimizer to use')
+    parser.add_argument('--optimizer', type=str, default='adam', choices=['sgd', 'adam'], help='Type of optimizer to use')
     args = parser.parse_args()
 
     return args
@@ -67,6 +70,10 @@ if __name__=='__main__':
         model = Net()
     elif args.model =="simple_convnet_2":
         model = ConvNet()
+    elif args.model == "resnet50":
+        model = resnet50_model()
+
+    print(model)
 
     # Setup optimizers and loss function
     if args.loss_function == 'cross-entropy':
@@ -84,40 +91,49 @@ if __name__=='__main__':
     print('Number of trainable parameters: ', pytorch_total_params)
 
     if args.problem_type == 'missing-labels':
-        # 1. Train the teacher network in a supervised manner
         train_size = int(0.8 * len(labeled_train_dataset))
         valid_size = len(labeled_train_dataset) - train_size
         train_dataset, valid_dataset = torch.utils.data.random_split(labeled_train_dataset, [train_size, valid_size])
         labeled_train_dataloader = DataLoader(train_dataset, args.batch_size)
         labeled_valid_dataloader = DataLoader(valid_dataset, args.batch_size)
-        trained_model = train_supervised(model, args.epochs, labeled_train_dataloader, labeled_valid_dataloader, loss_fn, optimizer,args.device)
+        # Perfom training from scratch if no model is provided
+        if not args.checkpoint_supervised_model:
+            # 1. Train the teacher network in a supervised manner
+            trained_model = train_supervised(model, args.epochs, labeled_train_dataloader, labeled_valid_dataloader, loss_fn, optimizer,args.device)
         
+            # Saved the trained model
+            torch.save(trained_model.state_dict(), os.path.join(experiment_dir, 'supervised_weights'))
+
+            # Load the model
+            model.load_state_dict(torch.load(os.path.join(experiment_dir, 'supervised_weights')))
+        else:
+            model.load_state_dict(torch.load(args.checkpoint_supervised_model))
+
+
         # Perform evaluation on supervised model
-        valid_acc, valid_loss = evaluate(trained_model, labeled_valid_dataloader, loss_fn, args.device)
+        valid_acc, valid_loss = evaluate(model, labeled_valid_dataloader, loss_fn, args.device)
         print('Valid Acc : {:.5f} | Valid Loss : {:.3f} '.format(valid_acc, valid_loss))
 
-        # Saved the trained model
-        torch.save(trained_model.state_dict(), os.path.join(experiment_dir, 'supervised_weights'))
+        if not args.checkpoint_semi_supervised_model:
+            # 2. Train the student model with respect to the weights learnt by the teacher model
+            semisupervised_trained_model= train_semisupervised(model, 
+                                                               args.epochs, 
+                                                               labeled_train_dataloader, 
+                                                               unlabeled_train_dataloader, 
+                                                               labeled_valid_dataloader, 
+                                                               loss_fn, 
+                                                               optimizer,
+                                                               args.device)
+            torch.save(semisupervised_trained_model.state_dict(), os.path.join(experiment_dir, 'semi_supervised_weights'))
+            model.load_state_dict(torch.load(os.path.join(experiment_dir, 'semi_supervised_weights')))
+        else:
+            model.load_state_dict(torch.load(args.checkpoint_semi_supervised_model))
 
-        # Load the model
-        model.load_state_dict(torch.load(os.path.join(experiment_dir, 'supervised_weights')))
-
-        # 2. Train the student model with respect to the weights learnt by the teacher model
-        semisupervised_trained_model= train_semisupervised(model, 
-                                                           args.epochs, 
-                                                           labeled_train_dataloader, 
-                                                           unlabeled_train_dataloader, 
-                                                           labeled_valid_dataloader, 
-                                                           loss_fn, 
-                                                           optimizer,
-                                                           args.device)
 
         # Perform evaluation of the model after the help of teacher model
-        valid_acc, valid_loss = evaluate(semisupervised_trained_model, labeled_valid_dataloader, loss_fn, args.device)
+        valid_acc, valid_loss = evaluate(model, labeled_valid_dataloader, loss_fn, args.device)
         print('Valid Acc : {:.5f} | Valid Loss : {:.3f} '.format(valid_acc, valid_loss))
-        torch.save(semisupervised_trained_model.state_dict(), os.path.join(experiment_dir, 'semi_supervised_weights'))
-
-        model.load_state_dict(torch.load(os.path.join(experiment_dir, 'semi_supervised_weights')))
+      
         # Perform prediction on test dataset and save the results
         img_paths, predictions = evaluate_for_submission(model, test_dataloader, device)
         results = {}
