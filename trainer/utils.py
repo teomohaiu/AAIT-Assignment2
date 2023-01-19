8ae9d34e6fc13bf17095f8443c919ac861298663
Mohaiu Teodora <mohaiuantoniateodora@yahoo.com>
2023-01-19 23:05:43 +0200

    Added EMAWeighted and SemiLoss for MixMatch and more helper functions.

--- trainer/utils.py
+++ trainer/utils.py
@@ -1,3 +1,6 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
 
 def alpha_weight(epoch):
     T1 = 100
@@ -9,3 +12,43 @@ def alpha_weight(epoch):
         return af
     else:
          return ((epoch-T1) / (T2-T1))*af
+
+class SemiLoss(object):
+    def __call__(self, outputs_x, targets_x, outputs_u, targets_u, lambda_u, epoch, nr_epochs):
+        probs_u = torch.softmax(outputs_u, dim=1)
+
+        Lx = -torch.mean(torch.sum(F.log_softmax(outputs_x, dim=1) * targets_x, dim=1))
+        Lu = torch.mean((probs_u - targets_u)**2)
+
+        return Lx, Lu, lambda_u * linear_rampup(epoch, nr_epochs)
+
+def linear_rampup(current, rampup_length):
+    if rampup_length == 0:
+        return 1.0
+    else:
+        current = np.clip(current / rampup_length, 0.0, 1.0)
+        return float(current)
+
+
+class WeightEMA(object):
+    def __init__(self, model, ema_model, lr, alpha=0.999):
+        self.model = model
+        self.ema_model = ema_model
+        self.alpha = alpha
+        self.params = list(model.state_dict().values())
+        self.ema_params = list(ema_model.state_dict().values())
+        self.wd = 0.02 * lr
+
+        for param, ema_param in zip(self.params, self.ema_params):
+            param.data.copy_(ema_param.data)
+
+    def step(self):
+        one_minus_alpha = 1.0 - self.alpha
+        for param, ema_param in zip(self.params, self.ema_params):
+            if ema_param.dtype==torch.float32:
+                ema_param.mul_(self.alpha)
+                ema_param.add_(param * one_minus_alpha)
+                # customized weight decay
+                param.mul_(1 - self.wd)
+
+ 
